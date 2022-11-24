fb29471df78e89ed1e4012dd08ed5d6e5a99c666
SebastianSotoAr <ssotoa@unal.edu.co>
2022-11-24 16:58:24 -0500

    try 17

--- preguntas.py
+++ preguntas.py
@@ -21,8 +21,8 @@ def pregunta_01():
     df = pd.read_csv("gm_2008_region.csv")
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
-    y = df["life"]
-    X = df["fertility"]
+    y = np.array(df["life"])
+    X = np.array(df["fertility"])
 
     # Imprima las dimensiones de `y`
     print(y.shape)
