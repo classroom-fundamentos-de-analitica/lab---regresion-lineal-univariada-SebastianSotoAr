9a5cb70be2d7ba3110d4040b796c70260a6a1ad3
SebastianSotoAr <ssotoa@unal.edu.co>
2022-11-23 23:36:07 -0500

    try 2

--- preguntas.py
+++ preguntas.py
@@ -50,22 +50,22 @@ def pregunta_02():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv("gm_2008_region.csv")
 
     # Imprima las dimensiones del DataFrame
-    print(____.____)
+    print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(____)
+    print("____")
 
     # Imprima la media de la columna `life` con 4 decimales.
-    print(____)
+    print("____")
 
     # Imprima el tipo de dato de la columna `fertility`.
-    print(____)
+    print("____")
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    print(____)
+    print("____")
 
 
 def pregunta_03():
